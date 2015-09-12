17eb21a1c1f6ad2de97723e601d3bd7ccd4e3092
Sayan Bhattacharya <bhattacharyasayan.21@gmail.com>
2015-09-12 06:28:17 +0530

    Few parameters changed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import csv
-with open('products(3).csv') as csvfile:
+with open('products.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		print(row['name'])
--- subprocess.py
+++ subprocess.py
@@ -3,6 +3,3 @@ with open('product.csv') as csvfile:
 	reader = csv.DictReader(csvfile)
 	for row in reader:
 		print(row['name'], row['price'])
-
-f = open('outputs.txt','w')
-f.write(row['name'])
